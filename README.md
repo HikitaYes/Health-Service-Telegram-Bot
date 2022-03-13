6c055a85a6ab2ef0fd9ec6ca749e71529ccc1240
Lenikas <stuffer232@gmail.com>
2022-03-13 12:00:21 +0500

    initialize spring project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Health-Service-Telegram-Bot
---
+++ src/main/java/com/example/healthbot/HealthBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthBotApplication.class, args);
+    }
+
+}
