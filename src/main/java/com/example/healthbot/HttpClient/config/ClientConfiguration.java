be6ae8b2bd7e4e84b7abd4095137931940bed8f1
Nikita Esyunin <hikita.yess@gmail.com>
2022-03-29 21:40:45 +0500

    Add http request logging

--- src/main/java/com/example/healthbot/HttpClient/config/ClientConfiguration.java
+++ src/main/java/com/example/healthbot/HttpClient/config/ClientConfiguration.java
@@ -3,15 +3,19 @@ package com.example.healthbot.HttpClient.config;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Configuration
 public class ClientConfiguration {
     private static final String BASE_URL = "https://www.e1.ru/health/pharma/";
@@ -29,7 +33,16 @@ public class ClientConfiguration {
 
         return WebClient.builder()
                 .baseUrl(BASE_URL)
+                .filter(logRequest())
                 .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
                 .build();
+    }
+
+    private static ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
+            clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
+            return Mono.just(clientRequest);
+        });
     }
 }
