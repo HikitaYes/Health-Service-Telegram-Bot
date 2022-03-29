f06b327df81950c54ea9c269becbb358fbb7112e
Nikita Esyunin <hikita.yess@gmail.com>
2022-03-30 00:56:38 +0500

    Add query params in HttpClient

--- src/main/java/com/example/healthbot/HttpClient/HttpClient.java
+++ src/main/java/com/example/healthbot/HttpClient/HttpClient.java
@@ -2,23 +2,33 @@ package com.example.healthbot.HttpClient;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriUtils;
 import reactor.core.publisher.Mono;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class HttpClient {
-    private static final String URL_PATH = "/";
     private final WebClient webClient;
 
-    public Mono<String> getPage() {
+    public Mono<String> getPage(String uri, MultiValueMap<String, String> map) {
         return webClient
                 .get()
-                .uri(URL_PATH)
+                .uri(uriBuilder -> uriBuilder
+                    .path(uri)
+                    .queryParams(map)
+                    .build()
+                )
                 .retrieve()
                 .onStatus(HttpStatus::is4xxClientError,
                         error -> Mono.error(new RuntimeException("Client side error while request")))
